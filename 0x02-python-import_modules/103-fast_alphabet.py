d5cc0fc0b4f07c807eda49829561079dc879f7e7
hanifanwar380 <moryn.mbugua@gmail.com>
2022-07-05 20:55:37 +0300

    Python tasks 3

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author: Martins Akhivbareme
+
+import string
+print(string.ascii_uppercase)
---
+++ 0x02-python-import_modules/variable_load_5.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author: Martins Akhivbareme
+
+a = 98
+"""Simple variable
+"""
