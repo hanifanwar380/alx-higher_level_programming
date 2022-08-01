11e67960395accfba7f9eb33d1188d7bf558b702
hanifanwar380 <hanifanwar380@gmail.com>
2022-08-01 23:27:12 +0300

    Python -Inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 11-square.py
-# Brennan D Baraban <375@holbertonschool.com>
+# Author : Hanif Miyanji
 """Defines a Rectangle subclass Square."""
 Rectangle = __import__('9-rectangle').Rectangle
 
